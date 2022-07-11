2b4b59d2ef957a1939b9202433f62ae21f8a41d2
Phat <phatdroid94@gmail.com>
2022-07-11 19:01:56 +0700

    Initial commit

---
+++ buoi1.dart
@@ -0,0 +1,27 @@
+import 'dart:ffi';
+
+void main() {
+
+  /**
+   * 1: Kiểu dữ liệu
+   *
+   */
+  // Primitive Types: Kiểu nguyên thủy (int, double, num)
+  // Object Types: Kiểu đối tượng (Như tài liệu)
+
+  /**
+   * 2: Khai báo biến
+   *
+   */
+  // Cách 1: Xác định kiểu dữ liệu cho đối tượng
+  // String name = "Phạm Tấn Phát";
+  // bool isMale = true;
+  // int weight = 75;
+  // double height = 1.75;
+  // Cách 2: Chưa xác định được giá trị
+  // dynamic value = 10;
+  // value = "10";
+  // String runesObject = "\u2026";
+  // print(runesObject);
+
+}
